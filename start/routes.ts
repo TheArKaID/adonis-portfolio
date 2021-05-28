c84e0ff44bbf4ae9a88581a751c73dc48c10c9bc
Arifia Kasastra R <Arka.progammer@gmail.com>
2021-05-28 15:33:39 +0700

    Moving GET / to FrontController

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('index')
+Route.get('/', 'FrontController.index')
+
+Route.get('/admin', async ({response})=>{
+  return response.send('Ok')
 })
