e4b635ec001d5ac0ad8568e5ff6108c40492afaa
Arifia Kasastra R <arka.progammer@gmail.com>
2021-05-30 15:55:28 +0700

    Finishing Auth

--- app/Controllers/Http/King/AuthController.ts
+++ app/Controllers/Http/King/AuthController.ts
@@ -1,38 +1,38 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { rules, schema } from "@ioc:Adonis/Core/Validator";
-import Config from 'App/Models/Config';
-export default class AuthController {
-    public async login({ view }: HttpContextContract) {
-        return view.render('login')
-    }
 
-    public async postLogin({ auth, response, request }: HttpContextContract) {
+export default class AuthController {
+    public async login({ view, auth, response }: HttpContextContract) {
         if (auth.isLoggedIn) {
-            console.log('ok')
             return response.redirect().toRoute('king.index')
         }
 
-        let validation = schema.create({
-            username: schema.string({trim: true}),
-            password: schema.string({trim: true}, [
-                rules.minLength(8)
-            ])
-        });
+        return view.render('login')
+    }
 
-        let validatedData = await request.validate({
-            schema: validation
-        });
+    public async postLogin({ auth, response, request, session }: HttpContextContract) {
+        if (auth.isLoggedIn) {
+            return response.redirect().toRoute('king.index')
+        }
 
-        // let config = Config.findBy({
-        //     username
-        // })
-        await auth.attempt(validatedData.username, validatedData.password)
+        try {
+            let validation = schema.create({
+                username: schema.string({ trim: true }),
+                password: schema.string({ trim: true }, [
+                    rules.minLength(8)
+                ])
+            });
 
-        return response.send(auth.isLoggedIn)
-        // try {
-        // } catch (error) {
-        //     return response.send(error)
-        //     // response.badRequest(error.messages)
-        // }
+            let validatedData = await request.validate({
+                schema: validation
+            });
+
+            await auth.attempt(validatedData.username, validatedData.password)
+
+            return response.redirect().toRoute('king.index')
+        } catch (error) {
+            session.flash({ 'errors': [error.message] })
+            return response.redirect().back()
+        }
     }
 }
