51c9e16eac84613e141b9bf3938ae153d86c796c
Arifia Kasastra R <arka.progammer@gmail.com>
2021-06-01 14:31:19 +0700

    Showing Statistic

--- app/Controllers/Http/King/KingController.ts
+++ app/Controllers/Http/King/KingController.ts
@@ -1,9 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Statistic from 'App/Models/Statistic'
 
 export default class AdminController {
-    public async index({ view, response, auth }: HttpContextContract) {
+    public async index({ view }: HttpContextContract) {
+        let statistic = await Statistic.first()
+
         return view.render('king.index', {
-            test: 'to mbro'
+            statistic: statistic
         })
     }
 }
