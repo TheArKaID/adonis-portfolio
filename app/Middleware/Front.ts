737f0992fc8f21ea0f4e1be41fb797438bd50d89
Arifia Kasastra R <Arka.progammer@gmail.com>
2021-06-02 12:25:55 +0700

    Updating Statistic logic

--- app/Middleware/Front.ts
+++ app/Middleware/Front.ts
@@ -1,29 +1,38 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Statistic from 'App/Models/Statistic'
+import Database from '@ioc:Adonis/Lucid/Database'
 import { DateTime } from 'luxon'
 
 export default class Front {
-  public async handle ({ session }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ session, request }: HttpContextContract, next: () => Promise<void>) {
     let visitDate = session.get('visit-date', false)
-    let now = new DateTime()
-    console.log(now)
-    // if(visitDate) {
-    //   if(now!=visitDate) {
-    //     session.put('visit-date', now)
+    let lastPage = session.get('last-page', false)
+    let now = DateTime.local().toISODate()
+    let stat = await Database.from('statistics').firstOrFail()
 
-    //     let stat = await Statistic.firstOrFail()
-    //     stat.views++
-    //     stat.visitors++
-    //     await stat.save()
-    //   }
-    // } else {
-    //   session.put('visit-date', now)
+    if (stat) {
+      if (visitDate) {
+        if (now != visitDate) {
+          session.put('visit-date', now)
 
-    //   let stat = await Statistic.firstOrFail()
-    //   stat.views++
-    //   stat.visitors++
-    //   await stat.save()
-    // }
+          await Database.from('statistics').increment({
+            visitors: 1
+          })
+        }
+      } else {
+        session.put('visit-date', now)
+
+        await Database.from('statistics').increment({
+          visitors: 1
+        })
+      }
+    }
+    if (lastPage != request.url()) {
+      session.put('last-page', request.url())
+
+      await Database.from('statistics').increment({
+        views: 1
+      })
+    }
 
     // code for middleware goes here. ABOVE THE NEXT CALL
     await next()
