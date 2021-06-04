44fc274794bd3e3a42dd33bcf8bb156c1ba9fd34
Arifia Kasastra R <Arka.progammer@gmail.com>
2021-06-04 16:30:10 +0700

    Creating Portfolio Mode, Migration n Controller

---
+++ app/Models/Portfolio.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Portfolio extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public platform: string
+  
+  @column()
+  public judul: string
+
+  @column()
+  public keterangan: string
+
+  @column()
+  public github: string
+
+  @column()
+  public web: string
+
+  @column()
+  public foto: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1622798490963_portfolios.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Portfolios extends BaseSchema {
+  protected tableName = 'portfolios'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('platform')
+      table.string('judul')
+      table.string('keterangan')
+      table.string('git').nullable()
+      table.string('web').nullable()
+      table.string('foto')
+      table.timestamps()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
