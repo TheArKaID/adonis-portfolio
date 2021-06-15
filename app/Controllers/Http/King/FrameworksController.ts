a0ddd13c77c8f350f46d8af29c5b7109b046e628
Arifia Kasastra R <Arka.progammer@gmail.com>
2021-06-15 09:18:03 +0700

    Creating add Framework

--- app/Controllers/Http/King/FrameworksController.ts
+++ app/Controllers/Http/King/FrameworksController.ts
@@ -1,14 +1,35 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { rules, schema } from "@ioc:Adonis/Core/Validator";
+import Framework from 'App/Models/Framework';
 
 export default class FrameworksController {
   public async index({ view }: HttpContextContract) {
-    return view.render('king.framework.index')
+    let frameworks = await Framework.all()
+
+    return view.render('king.framework.index', { frameworks })
   }
 
-  public async create({ }: HttpContextContract) {
+  public async create({ view }: HttpContextContract) {
+    return view.render('king.framework.create')
   }
 
-  public async store({ }: HttpContextContract) {
+  public async store({ request, response, session }: HttpContextContract) {
+    let validation = schema.create({
+      framework: schema.string({ escape: true, trim: true }, [
+        rules.required(),
+        rules.maxLength(255)
+      ])
+    })
+
+    let data = await request.validate({schema: validation})
+
+    await Framework.create({
+      name: data.framework
+    })
+
+    session.flash('success', 'Framework Added Successfully')
+
+    return response.redirect().toRoute('king.framework')
   }
 
   public async show({ }: HttpContextContract) {
