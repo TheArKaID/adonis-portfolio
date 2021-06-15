69599c91b0923f4e8f88cd771155dc0104bc2cbf
Arifia Kasastra R <arka.progammer@gmail.com>
2021-06-15 22:03:12 +0700

    Adding Messages model, table and controller

---
+++ app/Controllers/Http/MessagesController.ts
@@ -0,0 +1,15 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class MessagesController {
+  public async index ({}: HttpContextContract) {
+  }
+
+  public async show ({}: HttpContextContract) {
+  }
+
+  public async edit ({}: HttpContextContract) {
+  }
+
+  public async reply ({}: HttpContextContract) {
+  }
+}
