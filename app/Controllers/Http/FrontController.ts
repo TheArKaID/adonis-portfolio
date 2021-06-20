e5912715800aa74a65101911f1207d0ec6665e12
Arifia Kasastra R <arka.progammer@gmail.com>
2021-06-20 13:26:08 +0700

    Adding Front Message feature

--- app/Controllers/Http/FrontController.ts
+++ app/Controllers/Http/FrontController.ts
@@ -1,10 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { rules, schema } from "@ioc:Adonis/Core/Validator"
 import Education from 'App/Models/Education'
 import ProgrammingLanguage from 'App/Models/ProgrammingLanguage'
 import Profile from 'App/Models/Profile'
 import WorkExperience from 'App/Models/WorkExperience'
 import Framework from 'App/Models/Framework'
 import Portfolio from 'App/Models/Portfolio'
+import Message from 'App/Models/Message'
 
 export default class ConfigsController {
     public async index ({view}: HttpContextContract) {
@@ -22,4 +24,32 @@ export default class ConfigsController {
         return view.render('index')
     }
     
+    public async sendMessage({ request, response }: HttpContextContract) {
+        let validation = schema.create({
+            name: schema.string({ trim: true }, [
+                rules.required(),
+                rules.maxLength(255)
+            ]),
+            email: schema.string({ trim: true }, [
+                rules.required(),
+                rules.email()
+            ]),
+            subject: schema.string({ trim: true }, [
+                rules.required(),
+                rules.maxLength(255)
+            ]),
+            message: schema.string({ trim: true }),
+        })
+
+        let data = await request.validate({schema:validation})
+
+        await Message.create({
+            name: data.name,
+            email: data.email,
+            subject: data.subject,
+            message: data.message
+        })
+
+        return response.status(200).json(`OK`)
+    }
 }
